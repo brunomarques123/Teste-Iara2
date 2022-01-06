6e4251a617566cb0a48387c4d137376d3370d420
Bruno Marques <b_marques85@live.com>
2022-01-06 20:28:23 -0300

    Adicionar arquivos de projeto.

---
+++ ExemploArrayLinq/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace ExemploArrayLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numero = new int[11];
+
+            //leitura dos numeros informador pelo usuario
+            Console.WriteLine("Informe os números abaixo:");
+            for (int i = 0; i < 11; i++)
+            {
+                Console.Write("Numeros[" + i + "] :");
+                numero[i] = Convert.ToInt32(Console.ReadLine());
+            }
+                    
+            //percorre os numeros e contanto so os diferente de par   
+            int nimpar = (from n in numero select n).Where(n => n % 2 > 0).Count();
+
+            Console.WriteLine($"Deste total os números impares são {nimpar}");
+            Console.ReadLine();
+        }
+    }
+}
